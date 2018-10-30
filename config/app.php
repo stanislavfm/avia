35a50a12e908285b59754b9725303e75f1d3d7ca
stanislavfm <staschertilin@gmail.com>
2018-10-30 15:30:28 +0200

    Added IDE Helper

--- config/app.php
+++ config/app.php
@@ -151,6 +151,7 @@ return [
          * Package Service Providers...
          */
         Barryvdh\Debugbar\ServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
         /*
          * Application Service Providers...
